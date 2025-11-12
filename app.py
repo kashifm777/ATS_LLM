03c08c27c290975553993d3caf1f1ca18cc8da47
Muhammad Kashif <muhammadkashif841@gmail.com>
2025-11-12 10:06:48 +0500

    Update model to 'gemini-2.5-flash'

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv() ## load all our environment variables
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_repsonse(input):
-    model=genai.GenerativeModel('gemini-pro')
+    model=genai.GenerativeModel('gemini-2.5-flash')
     response=model.generate_content(input)
     return response.text
 
